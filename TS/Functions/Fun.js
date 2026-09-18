ff06cf8439bfeb58d451d4aa2f9fe4840b26b53d
changed2505-creator <changed2505@gmail.com>
2026-09-18 10:50:54 +0530

    Function ts(parameters and return annotations)

---
+++ TS/Functions/Fun.js
@@ -0,0 +1,42 @@
+"use strict";
+// function square(num: number) {
+//   return num * num;
+// }
+// function naming(n: string) {
+//   return `Hi ${n}`;
+// }
+// console.log(square(10));
+// console.log(naming("Hemanth"));
+// const addStringNo = (name: string, salary: number) => {
+//   return `${name}:${salary}`;
+// };
+// console.log(addStringNo("Arjun", 50000));
+// function add(a: number, b: number = 10) {
+//   return a + b;
+// }
+// console.log(add(20));
+// const sub = (num1: number, num2: number): number | string => {
+//   if (num1 - num2 < 0) {
+//     return (num1 - num2).toString();
+//   }
+//   return num1 - num2;
+// };
+// console.log(sub(2, 10));
+// console.log(sub(5, 1));
+// let arr = ["hi", "Bye", "Hello"];
+// let brr = arr.map((e) => {
+//   return e.toUpperCase();
+// });
+// console.log(brr);
+// function returnNothing(n: string): void {
+//   console.log(n);
+// }
+// returnNothing("Harish");
+function neverReturn(msg) {
+    throw new Error(msg);
+}
+function loopsInfinte(num) {
+    while (true) {
+        num + num;
+    }
+}
